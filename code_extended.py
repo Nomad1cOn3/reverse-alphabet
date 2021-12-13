41dd9e7efd7238b05e28c5b57828990db9a401b0
Nomad1cOn3 <bostroff2@gmail.com>
2021-12-13 01:24:19 +0000

    Testing

---
+++ code_extended.py
@@ -0,0 +1,6 @@
+alphabet_lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] #list of lowercase letters
+alphabet_upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] # list of uppercase letters
+index = -1 #list index control variable
+for item in alphabet_lower or alphabet_upper:
+  print(alphabet_lower[index],alphabet_upper[index])
+  index = index - 1 
